08db2633dd36ff5fc8f5e4b3516630a2447ad130
Jad Kik <jad.elkik@lightspeedhq.com>
2020-10-06 12:37:37 +0200

    Update hook

--- pre_commit_hooks/circleci-validate.sh
+++ pre_commit_hooks/circleci-validate.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "This pre-commit hook is DEPRECATED. Please see https://github.com/danielhoherd/pre-commit-circleci for more info."
-
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -9,13 +7,15 @@ set -o nounset
 DEBUG=${DEBUG:=0}
 [[ $DEBUG -eq 1 ]] && set -o xtrace
 
-exec < /dev/tty
-
 echo 'Begin circleci config validation'
 
 if ! command -v circleci &>/dev/null; then
-  >&2 echo 'circleci command not found. See https://circleci.com/docs/2.0/local-cli/ for installation instructions.'
+  if command -v brew &>/dev/null; then
+    brew install circleci
+  else
+    >&2 echo 'circleci command not found. See https://circleci.com/docs/2.0/local-cli/ for installation instructions.'
+  fi
   exit 1
 fi
 
-circleci config validate
+circleci --skip-update-check config validate
